70ef62195836a1f4fd3f09cd405b5eed8e41f0e5
Anthony Demott <java152001@gmail.com>
2018-10-17 23:45:19 -0700

    Added logic to post route to identify best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,7 +11,29 @@ module.exports = (function() {
     apiRoutes.post('/add', function (req, res) {
         var newPerson = req.body;
         var friends = require('../data/friends.js')
-        friends.friends.push(newPerson);
+        var scores = [];
+        var total = 0;
+        for (var i = 0; i < friends.friends.length; i++) {
+            for (var j = 0; j < friends.friends[i].scores.length; j++) {
+                var diff = parseInt(newPerson.scores[j]) - parseInt(friends.friends[i].scores[j]);
+                total = total + diff;
+            }
+            scores.push({"position": i, "total": Math.abs(total)});
+            total = 0;
+        }
+        
+        var value = scores[0].total;
+        var position = 0;
+        for (var i = 1; i < scores.length; i++) {
+            if (scores[i].total < value) {
+                value = scores[i].total;
+                position = scores[i].position;
+            }
+        }
+
+        res.send(friends.friends[position]);
+
+
     });
 
     return apiRoutes;
