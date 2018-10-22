564c4be9754dda9ba286718609a25cb96211d267
Anthony Demott <java152001@gmail.com>
2018-10-21 23:30:49 -0700

    Added to deploy final product

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,34 @@
 var friends = [
 {
-    "name": "Bethany",
-    "photo": "",
+    "name": "Light Yagami",
+    "photo": "https://ae01.alicdn.com/kf/HTB1bMZVNXXXXXcMaXXXq6xXFXXXl/Light-Yagami-Death-Note-Anime-Manga-Art-Huge-Print-Poster-TXHOME-D8052.jpg_640x640.jpg",
     "scores": [
-        5,
-        4,
-        3,
-        2,
         1,
         1,
-        2,
-        3,
-        4,
-        5
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
     ]
 },
 {
-    "name": "Danny",
-    "photo": "",
+    "name": "Naruto Uzamaki",
+    "photo": "https://www.tvovermind.com/wp-content/uploads/2018/08/Naruto-750x415-750x415.jpg",
     "scores": [
-        1,
-        2,
-        3,
-        4,
         5,
         5,
-        4,
-        3,
-        2,
-        1
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
     ]
 },
 {
@@ -45,6 +45,38 @@ var friends = [
         2,
         1,
         1
+    ]
+},
+{
+    "name": "Squirtle",
+    "photo": "https://s3.amazonaws.com/twin-galaxies-www/assets/article/2018/07/08/pokemon-go-sunglasses-squirtle-header_feature.jpg",
+    "scores": [
+        3,
+        2,
+        4,
+        5,
+        5,
+        4,
+        4,
+        3,
+        1,
+        1
+    ]
+},
+{
+    "name": "Pikachu",
+    "photo": "https://i.kym-cdn.com/entries/icons/mobile/000/019/677/selon_les_concepteurs_de_la_serie_pokemon_pikachu_n_est_pas_une_souris_9271.jpg",
+    "scores": [
+        2,
+        4,
+        5,
+        2,
+        1,
+        3,
+        2,
+        5,
+        4,
+        2
     ]
 }
 
